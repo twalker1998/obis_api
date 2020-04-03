9ca9d9f3cfd44711ec5854afdf203a87da24ee8c
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-04-03 01:01:37 -0500

    added access-control-allow-origin: * header

--- api/cors.py
+++ api/cors.py
@@ -2,4 +2,5 @@ class CorsMiddleware(object):
 	def process_response(self, req, resp):
 		resp["Access-Control-Allow-Headers"] = "*"
 		resp["Access-Control-Allow-Methods"] = "*"
+		resp["Access-Control-Allow-Origin"] = "*"
 		return resp
