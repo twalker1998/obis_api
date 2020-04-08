10ea1613ab37d50839959f6a9cecf117fefb56e3
root <root@obsvweb1.net.ou.edu>
2020-04-08 17:37:17 -0500

    Removing unnecessary CORS header

--- api/cors.py
+++ api/cors.py
@@ -2,5 +2,4 @@ class CorsMiddleware(object):
 	def process_response(self, req, resp):
 		resp["Access-Control-Allow-Headers"] = "*"
 		resp["Access-Control-Allow-Methods"] = "*"
-		resp["Access-Control-Allow-Origin"] = "*"
 		return resp
