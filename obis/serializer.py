e92115fce5d88a19bb2f5c5b84997bade41b1bd2
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-10-26 19:35:40 -0500

    Updated relational field with queryset attribute

--- obis/serializer.py
+++ obis/serializer.py
@@ -1,14 +1,12 @@
 from rest_framework import serializers
 
-from models import *
-# This is an example of how you can make your own serializer. Default seems to be working.
-# Left this only for examples.
+from obis.models import *
 
 class AcctaxSerializer(serializers.HyperlinkedModelSerializer):
-    family = serializers.SlugRelatedField(slug_field='family')
+    family = serializers.SlugRelatedField(slug_field='family', queryset=Acctax.objects.all())
     class Meta:
         model = Acctax
-	fields = ('url','a_id','acode','sname','scientificnameauthorship','family','genus','species','subspecies','variety','forma','elcode','gelcode','iucncode','g_rank','s_rank','nativity','source','usda_code','tsn','fed_status','st_status','swap','scientificname','sspscientificnameauthorship','varscientificnameauthorship','formascientificnameauthorship','tracked')
+        fields = ('url','a_id','acode','sname','scientificnameauthorship','family','genus','species','subspecies','variety','forma','elcode','gelcode','iucncode','g_rank','s_rank','nativity','source','usda_code','tsn','fed_status','st_status','swap','scientificname','sspscientificnameauthorship','varscientificnameauthorship','formascientificnameauthorship','tracked')
 
 class ComtaxSerializer(serializers.HyperlinkedModelSerializer):
     #primary_name = serializers.CharField(source='primary_name')
@@ -26,38 +24,38 @@ class HightaxSerializer(serializers.HyperlinkedModelSerializer):
     #url = serializers.HyperlinkedIdentityField(view_name="obis:hightax-detail")
     family = serializers.CharField(source='family')
     class Meta:
-         model = Hightax
-         fields = ('url','kingdom','phylum','taxclass','taxorder','family','category','name_type_desc','name_category_desc')
+        model = Hightax
+        fields = ('url','kingdom','phylum','taxclass','taxorder','family','category','name_type_desc','name_category_desc')
 
 class SyntaxSerializer(serializers.HyperlinkedModelSerializer):    
      class Meta:
-         model = Syntax
-         fields = ('url','s_id','acode','scode','sname','scientificnameauthorship','family','genus','species','subspecies','variety','scientificname','sspscientificnameauthorship','varscientificnameauthorship','formascientificnameauthorship','tsn')
+        model = Syntax
+        fields = ('url','s_id','acode','scode','sname','scientificnameauthorship','family','genus','species','subspecies','variety','scientificname','sspscientificnameauthorship','varscientificnameauthorship','formascientificnameauthorship','tsn')
 
 class OccurenceSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-	 model = Occurrence
-	 fields = ('url','resourcetype','gid','acode','eventdate','recordedby','county','locality','behavior','habitat','sex','lifestage','associatedtaxa','verbatimelevation','depth','depthaccuracy','individualcount','occurrenceremarks','taxonremarks','institutioncode','basisofrecord','catalognumber','othercatalognumbers','typestatus','recordnumber','samplingprotocol','preparations','primary_data','associatedreferences','datasetname','coordinateprecision','decimallatitude','decimallongitude','geodeticdatum','georeferencedby','georeferenceddate','georeferenceremarks','georeferencesources','georeferenceverificationstatus','geom','problem_with_record','previousidentifications','identificationverificationstatus','identificationconfidence','identificationremarks','datelastmodified','associatedoccurrences','associatedsequences','entby','entrydate','obs_gid','mtr','township','ns','range','ew','section','quarter','zone','utme','utmn','hiderecord','hiderecordcomment','relationshipremarks','informationwitheld','awaitingreview','occurrenceid')
+        model = Occurrence
+        fields = ('url','resourcetype','gid','acode','eventdate','recordedby','county','locality','behavior','habitat','sex','lifestage','associatedtaxa','verbatimelevation','depth','depthaccuracy','individualcount','occurrenceremarks','taxonremarks','institutioncode','basisofrecord','catalognumber','othercatalognumbers','typestatus','recordnumber','samplingprotocol','preparations','primary_data','associatedreferences','datasetname','coordinateprecision','decimallatitude','decimallongitude','geodeticdatum','georeferencedby','georeferenceddate','georeferenceremarks','georeferencesources','georeferenceverificationstatus','geom','problem_with_record','previousidentifications','identificationverificationstatus','identificationconfidence','identificationremarks','datelastmodified','associatedoccurrences','associatedsequences','entby','entrydate','obs_gid','mtr','township','ns','range','ew','section','quarter','zone','utme','utmn','hiderecord','hiderecordcomment','relationshipremarks','informationwitheld','awaitingreview','occurrenceid')
 
 class FedStatusSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-	 model = FedStatus
-	 fields = ('url','status_id','status','description')
+        model = FedStatus
+        fields = ('url','status_id','status','description')
 
 class StStatusSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-	 model = StStatus
-         fields = ('url','status_id','status','description')
+        model = StStatus
+        fields = ('url','status_id','status','description')
 
 class OkSwapSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-         model = OkSwap
-         fields = ('url','swap_id','tier','description')
+        model = OkSwap
+        fields = ('url','swap_id','tier','description')
 
 class InstitutionSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-         model = Institution
-         fields = ('url','institutioncode','institution','curator','email','telephone','address','city','state','country','zipcode','institutiontype','link')
+        model = Institution
+        fields = ('url','institutioncode','institution','curator','email','telephone','address','city','state','country','zipcode','institutiontype','link')
 
 class CountySerializer(serializers.HyperlinkedModelSerializer):
     county = serializers.CharField(source='county')
@@ -73,18 +71,18 @@ class IdentificationVerificationSerializer(serializers.HyperlinkedModelSerialize
 
 class CoTrsSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-         model = CoTrs
-         fields = ('url','gid','name','geom','trs')
+        model = CoTrs
+        fields = ('url','gid','name','geom','trs')
 
 class RankChangeSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-         model = RankChange
-         fields = ('url','r_id','acode','previous_s_rank','s_rank','changedby','rankremarks','datelastmodified','previousdatemodified')
+        model = RankChange
+        fields = ('url','r_id','acode','previous_s_rank','s_rank','changedby','rankremarks','datelastmodified','previousdatemodified')
 
 class SpatialRefSysSerializer(serializers.HyperlinkedModelSerializer):
      class Meta:
-         model = SpatialRefSys
-         fields = ('url','srid','auth_name','auth_srid','srtext','proj4text')
+        model = SpatialRefSys
+        fields = ('url','srid','auth_name','auth_srid','srtext','proj4text')
 
 class IUCNLookupSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.CharField(source='id')
