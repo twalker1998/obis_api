dae8d2a656d50e8f9b9f5acf01baac7c2b007b13
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-11-05 17:51:34 -0600

    fixed api/urls.py for django 2.1

--- api/urls.py
+++ api/urls.py
@@ -37,5 +37,5 @@ urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     # Admin Urls
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', admin.site.urls)
 ]
