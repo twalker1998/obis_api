3a114d0314b625f5452778bc1bfda9f307542635
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-11-13 19:18:21 -0600

    updated custom middleware classes

--- api/cors.py
+++ api/cors.py
@@ -1,4 +1,7 @@
 class CorsMiddleware(object):
+	def __init__(self, get_response):
+		self.get_response = get_response
+
 	def process_response(self, req, resp):
 		resp["Access-Control-Allow-Headers"] = "*"
 		resp["Access-Control-Allow-Methods"] = "*"
--- api/disable.py
+++ api/disable.py
@@ -1,4 +1,7 @@
 class DisableCSRF(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
     def process_request(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
 
