ef098a4a042ffff95aca333c765d03b39c3d7352
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-11-14 18:11:44 -0600

    updated url formats

--- api/settings.py
+++ api/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            (os.path.join(BASE_DIR, 'templates'),)
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
