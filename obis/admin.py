c7fe4a27f97fb4811dc4f44afdba66b015628624
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-11-18 13:25:57 -0600

    wrote admin module for obis

--- obis/admin.py
+++ obis/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.apps import apps
+from django.contrib.auth.management import create_permissions
 
-# Register your models here.
+app = apps.get_app_config('obis')
+create_permissions(app)
