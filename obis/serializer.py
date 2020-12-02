e03201163c3bd52c6b9f857d59d6765389cec4a0
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-12-01 21:42:00 -0600

    OCD

--- obis/serializer.py
+++ obis/serializer.py
@@ -118,7 +118,7 @@ class DRegularitySerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'd_regularity_id', 'regularity')
 
 class FedStatusSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = FedStatus
         fields = ('url', 'status_id', 'status', 'description')
 
@@ -146,7 +146,7 @@ class IdentificationVerificationSerializer(serializers.HyperlinkedModelSerialize
                   'identificationremarks', 'datalastmodified', 'identifiedacode', 'gid')
 
 class InstitutionSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = Institution
         fields = ('url', 'institutioncode', 'institution', 'curator', 'email', 'telephone',
                   'address', 'city', 'state', 'country', 'zipcode', 'institutiontype', 'link')
@@ -187,18 +187,18 @@ class NativityLookupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('n_id', 'nativity')
 
 class OccurenceSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = Occurrence
         fields = ('url', 'resourcetype', 'gid', 'acode', 'eventdate', 'recordedby', 'county', 'locality', 'behavior', 'habitat', 'sex', 'lifestage', 'associatedtaxa', 'verbatimelevation', 'depth', 'depthaccuracy', 'individualcount', 'occurrenceremarks', 'taxonremarks', 'institutioncode', 'basisofrecord', 'catalognumber', 'othercatalognumbers', 'typestatus', 'recordnumber', 'samplingprotocol', 'preparations', 'primary_data', 'associatedreferences', 'datasetname', 'coordinateprecision', 'decimallatitude', 'decimallongitude', 'geodeticdatum', 'georeferencedby',
                   'georeferenceddate', 'georeferenceremarks', 'georeferencesources', 'georeferenceverificationstatus', 'geom', 'problem_with_record', 'previousidentifications', 'identificationverificationstatus', 'identificationconfidence', 'identificationremarks', 'datelastmodified', 'associatedoccurrences', 'associatedsequences', 'entby', 'entrydate', 'obs_gid', 'mtr', 'township', 'ns', 'range', 'ew', 'section', 'quarter', 'zone', 'utme', 'utmn', 'hiderecord', 'hiderecordcomment', 'relationshipremarks', 'informationwitheld', 'awaitingreview', 'occurrenceid')
 
 class OkSwapSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = OkSwap
         fields = ('url', 'swap_id', 'tier', 'description')
 
 class RankChangeSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = RankChange
         fields = ('url', 'r_id', 'acode', 'previous_s_rank', 's_rank',
                   'changedby', 'rankremarks', 'datelastmodified', 'previousdatemodified')
@@ -216,7 +216,7 @@ class SourceSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'source', 'description')
 
 class SpatialRefSysSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = SpatialRefSys
         fields = ('url', 'srid', 'auth_name',
                   'auth_srid', 'srtext', 'proj4text')
@@ -229,12 +229,12 @@ class StateRankLookupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'code')
 
 class StStatusSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+    class Meta:
         model  = StStatus
         fields = ('url', 'status_id', 'status', 'description')
 
 class SyntaxSerializer(serializers.HyperlinkedModelSerializer):    
-     class Meta:
+    class Meta:
         model  = Syntax
         fields = ('url', 's_id', 'acode', 'scode', 'sname', 'scientificnameauthorship', 'family', 'genus', 'species', 'subspecies',
                   'variety', 'scientificname', 'sspscientificnameauthorship', 'varscientificnameauthorship', 'formascientificnameauthorship', 'tsn')
