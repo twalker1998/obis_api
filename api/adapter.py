adbd4e90965b16797c9d43adeee4bbe5ec739538
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-12-09 15:39:14 -0600

    Removed TODO

--- api/adapter.py
+++ api/adapter.py
@@ -5,7 +5,6 @@ class CustomDefaultAccountAdapter(DefaultAccountAdapter):
     """
     Extend the `DefaultAccountAdapter` class from `allauth.account.adapter`
     to allow the API to send registration emails.
-    TODO: comment
     """
     def send_mmail(self, template_prefix, email, context):
         context['activate_url'] = settings.URL_FRONT + 'verify/' + context['key']
